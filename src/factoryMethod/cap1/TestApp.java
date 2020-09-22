ee8bd958f7425c3e8da73c6a9673c0949a0fa668
bin.li <bin.li03@weimob.com>
2020-09-22 09:55:44 +0800

    fix

--- src/factoryMethod/cap1/TestApp.java
+++ src/factoryMethod/cap1/TestApp.java
@@ -1,10 +1,4 @@
 package factoryMethod.cap1;
-
-/**
- * @author 李彬
- * @description
- * @date 2020-09-21 16:09
- **/
 public class TestApp {
     public static void main(String[] args) {
         ExportFactory exportFactory = new PDFExportFactory();
--- src/factoryMethod/cap1/UserWordWordExport.java
+++ src/factoryMethod/cap1/UserWordWordExport.java
@@ -1,10 +1,6 @@
 package factoryMethod.cap1;
 
-/**
- * @author 李彬
- * @description
- * @date 2020-09-21 16:03
- **/
+
 public class UserWordWordExport implements ExportFile{
     @Override
     public void export() {
